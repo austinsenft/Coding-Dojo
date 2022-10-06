2079b13ed6181382d8319af5a39fb2d8b8523b99
Austin Senft <austinsenft@gmail.com>
2022-10-05 23:12:11 -0600

    Starting CRUD development

---
+++ Python/flask_mysql/crud/users_cr/server.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, redirect, request
+# import the class from friend.py
+from users import User
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/users')
+def table(): 
+    users = User.get_all()
+    print(users)
+    return render_template("read_all.html", list_of_users = users)
+
+# route to render form to add new users 
+@app.route('/user_form')
+def user_form():
+    return render_template("create.html")
+
+#route to create new users
+@app.route('/create_user', methods=["POST"])
+def create_user(): 
+    id = User.create(request.form)
+    return redirect(f"/users/{id}") 
+# Don't forget to redirect after saving to the database.
+
+#route to see one user
+@app.route('/users/<int:id>')
+def get_one(id): 
+    data = {'id': id}
+    user = User.get_one(data)
+    return render_template("read_one.html", user=user)
+#make sure 'user=user' section of return is done correctly, causes tons of issues here
+
+#route to see edit user template
+@app.route('/users/<int:id>/edit')
+def edit_one(id): 
+    data = {'id': id}
+    user = User.get_one(data)
+    return render_template("edit.html", user=user)
+
+#route to actually update/edit a user
+@app.route('/users/<int:id>/edited', methods=["POST"])
+def edit_user(id):
+    data = { 
+        'id' : id,
+        'fname': request.form["fname"], 
+        'lname': request.form["lname"], 
+        'email': request.form["email"]
+    }
+    User.edit(data)
+    return redirect(f"/users/{id}")
+
+
+#route to delete a user
+@app.route('/users/<int:id>/delete')
+def delete_user(id): 
+#make sure to add id into this method when deleting
+    data = {'id': id}
+    User.delete(data)
+    return redirect('/')
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
