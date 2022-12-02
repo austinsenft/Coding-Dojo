d48d98cebe389a2efbc5100f51414012c5a53175
Austin Senft <austinsenft@gmail.com>
2022-12-02 13:57:23 -0700

    Routing

--- Java/algos/week1/sll.js
+++ Java/algos/week1/sll.js
@@ -167,6 +167,107 @@ class SLL {
         }
         return this.containsRecursive(val, current.next);
     }
+
+    // FRIDAY ----------------------------------------------------------------------//
+
+    /**
+     * Removes the node that has the matching given val as it's data.
+     * - Time: (?).
+     * - Space: (?).
+     * @param {any} val The value to compare to the node's data to find the
+     *    node to be removed.
+     * @returns {boolean} Indicates if a node was removed or not.
+     */
+
+
+    removeVal(val) {
+        // use contains method
+        if (this.contains(val)) {
+
+            // if it at start, removeAtFront
+            if (this.head.value == val) {
+                this.removeAtFront()
+                return true;
+            }
+
+            let runner = this.head;
+            while (runner.next.next !== null) {
+                runner = runner.next
+
+                // if it in middle, we need to point runner.next to runner.next.next
+                if (runner.next.value == val) {
+                    runner.next = runner.next.next
+                    return true;
+                }
+
+                // // if it at end, removeAtBack
+                //     this.removeBack()
+                //     return true; 
+
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Retrieves the data of the second to last node in this list.
+     * - Time: (?).
+     * - Space: (?).
+     * @returns {any} The data of the second to last node or null if there is no
+     *    second to last node.
+     */
+    // retrieves 2nd to last data in node
+    secondToLast() {
+        // SLL IS EMPTY
+        if (this.isEmpty()) {
+            return null;
+        }
+        // SLL HAS ONE
+        if (this.head.next === null) {
+            console.log(`SLL DOESN'T HAVE 2 VALUES`)
+            return null;
+        }
+        // SLL IS NOT EMPTY
+        let runner = this.head;
+        while (runner.next.next.next !== null) {
+            runner = runner.next;
+        }
+        return runner.next.value;
+    }
+
+    //  FRIDAY EXTRA
+    /**
+     * Retrieves the data of the kth to last node in this list.
+     * - Time: (?).
+     * - Space: (?).
+     * @returns {any} The data of the kth to last node or null if there is no
+     *    kth to last node.
+     */
+    kthToLast(k) {
+        // check if empty
+        if (this.isEmpty()) {
+            return null
+        }
+        var length = 1
+        let runner = this.head
+        // determine length of the list
+        while (runner.next != null) {
+            length++
+            runner = runner.next
+        }
+        // check to see if the requested node exists, exit if it does not
+        if (length - 1 < k) {
+            return null
+        }
+        runner = this.head
+        // otherwise, move through list to the proper node and return the value
+        for (var i = 0; i < length - k - 1; i++) {
+            runner = runner.next
+        }
+        return runner.value
+
+    }
+
 }
 
 // TESTING
@@ -176,14 +277,16 @@ sll1.insertFromBack(2)
 sll1.insertFromBack(3)
 sll1.insertFromBack(4)
 
-sll1.insertFromFront(0)
-sll1.removeAtFront()
-sll1.average()
-sll1.printValues()
+// sll1.insertFromFront(0)
+// sll1.removeAtFront()
+// sll1.average()
+// sll1.printValues()
 
-sll1.removeBack(4)
-console.log(sll1.contains(4))
-sll1.containsRecursive(4)
+// sll1.removeBack(4)
+// console.log(sll1.contains(4))
+// sll1.containsRecursive(4)
+console.log(sll1.removeVal(4))
+sll1.printValues()
 
 
 console.log('~~~~~~~~~')
